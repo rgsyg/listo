6d8867fa5d96bfeb1ba06633c52b22482fcd4342
rigs <rigorsyguatbsn@gmail.com>
2025-06-14 16:17:06 +0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import "dotenv/config"
+import express from "express"
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:" + PORT)
+})
