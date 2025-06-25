d30a4aefb1a8d6ebbad7b03cc18c20d52dac1c15
Rigor <rigorsyguatbsn@gmail.com>
2025-06-25 15:09:43 +0800

    refactor card component

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
-import useTodoStore, { type Priority, type Todo } from "../store/useTodoStore";
+import useTodoStore, { type Todo } from "../store/useTodoStore";
 import { useLocation } from "react-router-dom";
-import Modal from "bootstrap/js/dist/modal";
 import Masonry from "react-masonry-css";
+import TodoCard from "./TodoCard";
 
 const breakpointColumnsObj = {
   default: 3,
@@ -12,19 +12,7 @@ const breakpointColumnsObj = {
 
 export default function MainContent() {
   const { pathname } = useLocation();
-
-  const { todos, fetchTodos, currentTodo, updateTodo, deleteTodo } =
-    useTodoStore();
-
-  const createBadge = (priority: Priority) => {
-    if (priority === "low") {
-      return <span className="badge text-bg-info">Low</span>;
-    } else if (priority === "med") {
-      return <span className="badge text-bg-warning">Med</span>;
-    } else {
-      return <span className="badge text-bg-danger">High</span>;
-    }
-  };
+  const { todos, fetchTodos } = useTodoStore();
 
   useEffect(() => {
     fetchTodos();
@@ -50,116 +38,7 @@ export default function MainContent() {
             </div>
             {todos.map(
               (todo: Todo) =>
-                !todo.archived &&
-                !todo.is_completed && (
-                  <div
-                    className="card p-3 fw-bold border border-black border-2 fs-5"
-                    style={{ boxShadow: "8px 6px" }}
-                    key={todo.id}
-                  >
-                    <div className="card-body p-0">
-                      <h5 className="card-title">
-                        <span className="fw-bold">{todo.title}</span>
-                      </h5>
-                      <div className="d-flex justify-content-between align-items-center fs-6">
-                        {createBadge(todo.priority)}
-                        <p
-                          className="text-muted m-0 fw-normal"
-                          style={{ fontSize: "0.90rem" }}
-                        >
-                          {"Created on " +
-                            new Date(todo.created_at)
-                              .toLocaleString()
-                              .split(",")[0]}
-                        </p>
-                      </div>
-                      <p className="card-text fw-normal pb-3 pt-2">
-                        {todo.description}
-                      </p>
-                      <div className="row g-0 align-items-end">
-                        <div className="col d-flex gap-3">
-                          <button
-                            className="btn btn-light border border-2 border-black"
-                            style={{ padding: "0.10rem 0.45rem" }}
-                            onClick={() =>
-                              updateTodo({
-                                ...todo,
-                                is_completed: !todo.is_completed,
-                              })
-                            }
-                          >
-                            <i
-                              className="bi bi-check2"
-                              style={{ fontSize: "1rem" }}
-                            />
-                          </button>
-                          <button
-                            className="btn btn-light border border-2 border-black"
-                            style={{ padding: "0.10rem 0.45rem" }}
-                            onClick={() => {
-                              currentTodo(todo.id);
-                              const updateModal =
-                                document.getElementById("updateModal");
-                              if (updateModal) {
-                                const modal =
-                                  Modal.getOrCreateInstance(updateModal);
-                                modal.show();
-                              }
-                            }}
-                          >
-                            <i
-                              className="bi bi-pencil"
-                              style={{ fontSize: "1rem" }}
-                            />
-                          </button>
-                          <div className="dropdown">
-                            <button
-                              className="btn btn-light border border-2 border-black dropdown-toggle"
-                              data-bs-toggle="dropdown"
-                              style={{ padding: "0.10rem 0.45rem" }}
-                            >
-                              <i
-                                className="bi bi-three-dots"
-                                style={{ fontSize: "1rem" }}
-                              />
-                            </button>
-                            <ul className="dropdown-menu">
-                              <li>
-                                <button
-                                  className="dropdown-item"
-                                  onClick={() =>
-                                    updateTodo({ ...todo, archived: true })
-                                  }
-                                >
-                                  Archive
-                                </button>
-                              </li>
-                              <li>
-                                <button
-                                  className="btn btn-light dropdown-item"
-                                  onClick={() => deleteTodo(todo.id)}
-                                >
-                                  Delete
-                                </button>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                        <div
-                          className="col text-end text-muted fw-normal"
-                          style={{ fontSize: "0.90rem" }}
-                        >
-                          {todo.due_date
-                            ? "Due on " +
-                              new Date(todo.due_date)
-                                .toLocaleString()
-                                .split(",")[0]
-                            : "No deadline"}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )
+                !todo.archived && !todo.is_completed && <TodoCard todo={todo} />
             )}
           </div>
           <div className="col m-0 d-flex flex-column gap-4">
@@ -178,116 +57,7 @@ export default function MainContent() {
               COMPLETE
             </div>
             {finishedTodos.map(
-              (todo: Todo) =>
-                !todo.archived && (
-                  <div
-                    className="card p-3 fw-bold border border-black border-2 fs-5"
-                    style={{ boxShadow: "8px 6px" }}
-                    key={todo.id}
-                  >
-                    <div className="card-body p-0">
-                      <h5 className="card-title">
-                        <span className="fw-bold">{todo.title}</span>
-                      </h5>
-                      <div className="d-flex justify-content-between align-items-center fs-6">
-                        {createBadge(todo.priority)}
-                        <p
-                          className="text-muted m-0 fw-normal"
-                          style={{ fontSize: "0.90rem" }}
-                        >
-                          {"Created on " +
-                            new Date(todo.created_at)
-                              .toLocaleString()
-                              .split(",")[0]}
-                        </p>
-                      </div>
-                      <p className="card-text fw-normal pb-3 pt-2">
-                        {todo.description}
-                      </p>
-                      <div className="row g-0 align-items-end">
-                        <div className="col d-flex gap-3">
-                          <button
-                            className="btn btn-light border border-2 border-black"
-                            style={{ padding: "0.10rem 0.45rem" }}
-                            onClick={() =>
-                              updateTodo({
-                                ...todo,
-                                is_completed: !todo.is_completed,
-                              })
-                            }
-                          >
-                            <i
-                              className="bi bi-check2"
-                              style={{ fontSize: "1rem" }}
-                            />
-                          </button>
-                          <button
-                            className="btn btn-light border border-2 border-black"
-                            style={{ padding: "0.10rem 0.45rem" }}
-                            onClick={() => {
-                              currentTodo(todo.id);
-                              const updateModal =
-                                document.getElementById("updateModal");
-                              if (updateModal) {
-                                const modal =
-                                  Modal.getOrCreateInstance(updateModal);
-                                modal?.show();
-                              }
-                            }}
-                          >
-                            <i
-                              className="bi bi-pencil"
-                              style={{ fontSize: "1rem" }}
-                            />
-                          </button>
-                          <div className="dropdown">
-                            <button
-                              className="btn btn-light border border-2 border-black dropdown-toggle"
-                              data-bs-toggle="dropdown"
-                              style={{ padding: "0.10rem 0.45rem" }}
-                            >
-                              <i
-                                className="bi bi-three-dots"
-                                style={{ fontSize: "1rem" }}
-                              />
-                            </button>
-                            <ul className="dropdown-menu">
-                              <li>
-                                <button
-                                  className="btn btn-light dropdown-item"
-                                  onClick={() =>
-                                    updateTodo({ ...todo, archived: true })
-                                  }
-                                >
-                                  Archive
-                                </button>
-                              </li>
-                              <li>
-                                <button
-                                  className="btn btn-light dropdown-item"
-                                  onClick={() => deleteTodo(todo.id)}
-                                >
-                                  Delete
-                                </button>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                        <div
-                          className="col text-end text-muted fw-normal"
-                          style={{ fontSize: "0.90rem" }}
-                        >
-                          {todo.due_date
-                            ? "Due on " +
-                              new Date(todo.due_date)
-                                .toLocaleString()
-                                .split(",")[0]
-                            : "No deadline"}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                )
+              (todo: Todo) => !todo.archived && <TodoCard todo={todo} />
             )}
           </div>
         </div>
@@ -300,93 +70,7 @@ export default function MainContent() {
           columnClassName="my-masonry-grid_column"
         >
           {archivedTodos.map((todo: Todo) => (
-            <div
-              className="card p-3 fw-bold border border-black border-2 fs-5"
-              style={{ boxShadow: "8px 6px" }}
-              key={todo.id}
-            >
-              <div className="card-body p-0">
-                <h5 className="card-title">
-                  <span className="fw-bold">{todo.title}</span>
-                </h5>
-                <div className="d-flex justify-content-between align-items-center fs-6">
-                  {createBadge(todo.priority)}
-                  <p
-                    className="text-muted m-0 fw-normal"
-                    style={{ fontSize: "0.90rem" }}
-                  >
-                    {"Created on " +
-                      new Date(todo.created_at).toLocaleString().split(",")[0]}
-                  </p>
-                </div>
-                <p className="card-text fw-normal pb-3 pt-2">
-                  {todo.description}
-                </p>
-                <div className="row g-0 align-items-end">
-                  <div className="col d-flex gap-3">
-                    <button
-                      className="btn btn-light border border-2 border-black"
-                      style={{ padding: "0.10rem 0.45rem" }}
-                      onClick={() => {
-                        currentTodo(todo.id);
-                        const updateModal =
-                          document.getElementById("updateModal");
-                        if (updateModal) {
-                          const modal = Modal.getOrCreateInstance(updateModal);
-                          modal?.show();
-                        }
-                      }}
-                    >
-                      <i
-                        className="bi bi-pencil"
-                        style={{ fontSize: "1rem" }}
-                      />
-                    </button>
-                    <div className="dropdown">
-                      <button
-                        className="btn btn-light border border-2 border-black dropdown-toggle"
-                        data-bs-toggle="dropdown"
-                        style={{ padding: "0.10rem 0.45rem" }}
-                      >
-                        <i
-                          className="bi bi-three-dots"
-                          style={{ fontSize: "1rem" }}
-                        />
-                      </button>
-                      <ul className="dropdown-menu">
-                        <li>
-                          <button
-                            className="btn btn-light dropdown-item"
-                            onClick={() =>
-                              updateTodo({ ...todo, archived: false })
-                            }
-                          >
-                            Unarchive
-                          </button>
-                        </li>
-                        <li>
-                          <button
-                            className="btn btn-light dropdown-item"
-                            onClick={() => deleteTodo(todo.id)}
-                          >
-                            Delete
-                          </button>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                  <div
-                    className="col text-end text-muted fw-normal"
-                    style={{ fontSize: "0.90rem" }}
-                  >
-                    {todo.due_date
-                      ? "Due on " +
-                        new Date(todo.due_date).toLocaleString().split(",")[0]
-                      : "No deadline"}
-                  </div>
-                </div>
-              </div>
-            </div>
+            <TodoCard todo={todo} />
           ))}
         </Masonry>
       )}
